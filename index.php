bed7a09abbbe7b12d890959e550ae4b9bd1ffed8
Angelique-Smit <ajd.smit@gmail.com>
2023-02-21 14:44:34 +0100

    First commmit

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-<?php
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <section class="nav_bar">
+            <div class="name">
+                <h1> Angelique Smit</h1>
+            </div>
+            <div class="links">
+                <a class="links1" href=""> About</a>
+                <a class="links2" href=""> Projects</a>
+                <a class="links3" href=""> Contact </a>
+            </div>
+        </section>
+    </nav>
+
+    <header>
+        <section class="slideshow_container">
+            <div id="slideshow">
+                <div class="slide-wrapper">
+                    <div class="slide"> </div>
+                    <div class="slide"> </div>
+                    <div class="slide"> </div>
+                </div>
+            </div>
+        </section>
+    </header>
+</body>
+</html>
